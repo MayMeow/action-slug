df3916d796e1b5cd90090a81092bf6848e4c091b
May <may@maymeow.com>
2024-01-15 14:40:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const sets = [
+    {to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶἀ]'},
+    {to: 'c', from: '[ÇĆĈČ]'},
+    {to: 'd', from: '[ÐĎĐÞ]'},
+    {to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]'},
+    {to: 'g', from: '[ĜĞĢǴ]'},
+    {to: 'h', from: '[ĤḦ]'},
+    {to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]'},
+    {to: 'j', from: '[Ĵ]'},
+    {to: 'ij', from: '[Ĳ]'},
+    {to: 'k', from: '[Ķ]'},
+    {to: 'l', from: '[ĹĻĽŁ]'},
+    {to: 'm', from: '[Ḿ]'},
+    {to: 'n', from: '[ÑŃŅŇ]'},
+    {to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]'},
+    {to: 'oe', from: '[Œ]'},
+    {to: 'p', from: '[ṕ]'},
+    {to: 'r', from: '[ŔŖŘ]'},
+    {to: 's', from: '[ßŚŜŞŠȘ]'},
+    {to: 't', from: '[ŢŤ]'},
+    {to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]'},
+    {to: 'w', from: '[ẂŴẀẄ]'},
+    {to: 'x', from: '[ẍ]'},
+    {to: 'y', from: '[ÝŶŸỲỴỶỸ]'},
+    {to: 'z', from: '[ŹŻŽ]'},
+    {to: '-', from: '[·/_,:;\']'}
+];
+
 try {
     const text = core.getInput('text');
-    const slug = text.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+
+    sets.forEach(set => {
+        text = text.replace(new RegExp(set.from, 'gi'), set.to);
+    });
+
+    const slug = text.toLowerCase().trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^-a-zа-я\u0370-\u03ff\u1f00-\u1fff]+/g, '')
+        .replace(/--+/g, '-')
+        .replace(/^-+/, '')
+        .replace(/-+$/, '');
 
     console.log(`Text: ${text}`);
     console.log(`Slug: ${slug}`);
